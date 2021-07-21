dc0e6b286cbb4aa7fab9e65d36e8cca78ced17df
hoa1999ptit <64449592+hoa1999ptit@users.noreply.github.com>
2021-07-22 01:24:30 +0700

    update

--- 1.UI/js/page/employee.js
+++ 1.UI/js/page/employee.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
+
     $('#add-employee-btn').click(function() {
         $('#detail-employee').css('display', 'block')
     })
-})
+
+
+
+
+
+});
+
+
+
+function loadData() {
+    // clear dữ liệu cũ trong bảng
+    $('tbody').empty();
+
+    // thực hiện load dữ liệu
+    //
+
+}
+
+
+
+function btnAddOnClick() {
+
+}
